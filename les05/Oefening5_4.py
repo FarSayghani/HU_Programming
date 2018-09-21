b205b0b7987027d82ac32d27a44f3c393bff0593
FarSayghani <Farhad_sayghani@msn.com>
2018-09-21 11:12:16 +0200

    Programming

---
+++ les05/Oefening5_4.py
@@ -0,0 +1,15 @@
+getallenrij = [1, 3, 5, 7, 9, 11, 13, 15]
+
+aantalDoorDrie = 0
+
+for getal in getallenrij:
+    if getal % 3 == 0:
+        aantalDoorDrie = aantalDoorDrie +1
+        
+
+
+
+
+print ('het aantal getallen deelbaar door drie is ' + str(aantalDoorDrie))
+
+
